c7dbc51c11b657f7e2f953dbb4d3e50672336e41
Maor Samuel <maordohifat@gmail.com>
2025-03-31 18:35:18 +0300

    Add files via upload

---
+++ my projects/Project 2 SQL/Project 2 guide.sql
@@ -0,0 +1,7 @@
+------------project 2
+
+/*>question 5 we have somting similar in the other fiels
+  >question 8 use union all and have super grand total that sum all the totals of all the year to one price
+  >question 10 dont use xml path .........
+  >HumanResorces.EmployeeDepartmentHistory inner join to bring the name
+  >question 2 connect Sales.Customer to Person.Person table and bring PersonID use left join */
